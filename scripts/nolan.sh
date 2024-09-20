82e98c41f4c064485e20673cb12189e4b63cdf26
nulxn <oioioihomelander@yahoo.com>
2024-09-19 21:54:25 -0700

    curl

--- scripts/nolan.sh
+++ scripts/nolan.sh
@@ -2,17 +2,17 @@
 
 BASE_URL="https://raw.githubusercontent.com/nulxn/nolan_2025/main"
 
-wget -O _notebooks/2024-09-15-itunes-y-table.ipynb \
-    "$BASE_URL/_notebooks/2024-09-15-itunes-y-table.ipynb"
+curl -o _notebooks/2024-09-15-itunes-y-table.ipynb \
+     "$BASE_URL/_notebooks/2024-09-15-itunes-y-table.ipynb"
 
-wget -O navigation/calc.md \
-    "$BASE_URL/navigation/calc.md"
+curl -o navigation/calc.md \
+     "$BASE_URL/navigation/calc.md"
 
-wget -O navigation/cookie.md \
-    "https://gist.githubusercontent.com/nulxn/6c532de73da755d0efcfd085c5cc8ac9/raw/6656818cc94cf1fb346ccd4d79b8b315480b3a43/cookie.md"
+curl -o navigation/cookie.md \
+     "https://gist.githubusercontent.com/nulxn/6c532de73da755d0efcfd085c5cc8ac9/raw/6656818cc94cf1fb346ccd4d79b8b315480b3a43/cookie.md"
 
-wget -O assets/ka-ching.mp3 \
-    "https://github.com/nulxn/nolan_2025/raw/main/images/brown/ka-ching.mp3"
+curl -o assets/ka-ching.mp3 \
+     "https://github.com/nulxn/nolan_2025/raw/main/images/brown/ka-ching.mp3"
 
 echo "Files downloaded:"
 echo "- _notebooks/2024-09-15-itunes-y-table.ipynb"
